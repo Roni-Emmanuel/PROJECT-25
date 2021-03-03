fa951d3f78457d790fd390a1342c3083650363e9
Roni-Emmanuel <74718809+Roni-Emmanuel@users.noreply.github.com>
2021-03-03 08:48:46 +0530

    Add files via upload

---
+++ project-25-main/paper.js
@@ -0,0 +1,35 @@
+var pap;
+class paper {
+    constructor(x, y) {
+      var options = {
+          isStatic:true,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2,
+      }
+      this.body = Bodies.circle(x, y, 100, options);
+      this.width = 100;
+      this.height = 50;
+      this.image = loadImage("paper.png")
+      
+     World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      
+      }
+
+      throwPaper(){
+        this.body.position.x = 1200;
+        this.body.position.y = 625;
+        this.display();
+      }
+
+
+  };
